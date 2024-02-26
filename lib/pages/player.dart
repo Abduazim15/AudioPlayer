fdee19dca091fda00c3be48bb047ae4c8c07e10f
Abduazim <qoribola5885@gmail.com>
2024-02-26 23:24:33 +0500

    UI changes and Timer Counter added

--- lib/pages/player.dart
+++ lib/pages/player.dart
@@ -1,11 +1,12 @@
+import 'dart:async';
+
 import 'package:abookplayer/AudioService.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:neumorphic_ui/neumorphic_ui.dart';
 import '../models/audio_model.dart';
 
 class PlayerPage extends StatefulWidget {
   const PlayerPage({super.key, required this.model});
+
   final AudioBookModel model;
 
   @override
@@ -15,19 +16,43 @@ class PlayerPage extends StatefulWidget {
 class _PlayerPageState extends State<PlayerPage> {
   final AudioBookModel model;
   bool isPlaying = true;
+  Duration duration = Duration.zero;
   final AudioService service = AudioService();
   _PlayerPageState({required this.model});
+  late Timer timer;
+  double sliderValue = 0;
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if(duration >= service.duration){
+        stopTimer();
+        return;
+      }
+      setState(() {
+        duration += const Duration(seconds: 1);
+        sliderValue += 100/service.duration.inSeconds;
+      });
+    });
+  }
+  void stopTimer(){
+    timer.cancel();
+  }
+
   @override
   void initState() {
     service.setAudio(model.audioPath);
     service.play();
+    startTimer();
     super.initState();
   }
+
   @override
   void dispose() {
     service.stop();
+    timer.cancel();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     Color adoptedColor = Theme.of(context).colorScheme.background;
@@ -44,8 +69,7 @@ class _PlayerPageState extends State<PlayerPage> {
           children: [
             Neumorphic(
               padding: const EdgeInsets.all(15),
-              style: NeumorphicStyle(
-                  color: adoptedColor),
+              style: NeumorphicStyle(color: adoptedColor),
               child: Column(
                 children: [
                   ClipRRect(
@@ -84,9 +108,9 @@ class _PlayerPageState extends State<PlayerPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('0:00'),
-                Icon(Icons.shuffle),
-                Icon(Icons.repeat),
+                Text(duration.toString()),
+                const Icon(Icons.shuffle),
+                const Icon(Icons.repeat),
                 Text(service.duration.toString()),
               ],
             ),
@@ -97,54 +121,63 @@ class _PlayerPageState extends State<PlayerPage> {
                 child: Slider(
                   min: 0,
                   max: 100,
-                  value: 50,
+                  value: sliderValue,
                   activeColor: Colors.green,
-                  onChanged: (value) {},
+                  onChanged: (value) {
+                    setState(() {
+                      sliderValue = value;
+                    });
+                  },
+
                 )),
             const SizedBox(height: 20),
-            Row(children: [
-              Expanded(
-                flex: 1,
-                child: GestureDetector(
-                  onTap: (){},
-                  child: Neumorphic(
-                    padding: const EdgeInsets.all(10),
-                    style: NeumorphicStyle(color: adoptedColor),
-                    child: const Icon(Icons.skip_previous, size: 30),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Neumorphic(
+                      padding: const EdgeInsets.all(10),
+                      style: NeumorphicStyle(color: adoptedColor),
+                      child: const Icon(Icons.skip_previous, size: 30),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 20),
-              Expanded(
-                flex: 2,
-                child: GestureDetector(
-                  onTap: (){
-                    setState(() {
-                      isPlaying ? service.stop() : service.play();
-                      isPlaying = !isPlaying;
-                    });
+                const SizedBox(width: 20),
+                Expanded(
+                  flex: 2,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
 
-                  },
-                  child: Neumorphic(
-                    padding: const EdgeInsets.all(10),
-                    style: NeumorphicStyle(color: adoptedColor),
-                    child: isPlaying ? const Icon(Icons.pause, size: 30) : const Icon(Icons.play_arrow, size: 30),
+                        isPlaying ? {service.stop(),stopTimer()} : {service.play(), startTimer()};
+                        isPlaying = !isPlaying;
+                      });
+                    },
+                    child: Neumorphic(
+                      padding: const EdgeInsets.all(10),
+                      style: NeumorphicStyle(color: adoptedColor),
+                      child: isPlaying
+                          ? const Icon(Icons.pause, size: 30)
+                          : const Icon(Icons.play_arrow, size: 30),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 20),
-              Expanded(
-                flex: 1,
-                child: GestureDetector(
-                  onTap: (){},
-                  child: Neumorphic(
-                    padding: const EdgeInsets.all(10),
-                    style: NeumorphicStyle(color: adoptedColor),
-                    child: const Icon(Icons.skip_next, size: 30),
+                const SizedBox(width: 20),
+                Expanded(
+                  flex: 1,
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Neumorphic(
+                      padding: const EdgeInsets.all(10),
+                      style: NeumorphicStyle(color: adoptedColor),
+                      child: const Icon(Icons.skip_next, size: 30),
+                    ),
                   ),
                 ),
-              ),
-            ],)
+              ],
+            )
           ],
         ),
       ),
